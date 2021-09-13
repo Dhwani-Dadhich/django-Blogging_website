386e9d14b798640b4e92acdf5f4b9d6266c08a4a
Dhwani-Dadhich <84697967+Dhwani-Dadhich@users.noreply.github.com>
2021-09-13 11:54:58 +0530

    Update settings.py

--- techfluenzer/settings.py
+++ techfluenzer/settings.py
@@ -146,8 +146,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS= True
 
 EMAIL_HOST= 'smtp.gmail.com' #simple mail transfer protocol server            #when someone registers then it sends email to that person
-EMAIL_HOST_USER= 'priyaaadoshi99999@gmail.com'
-EMAIL_HOST_PASSWORD= 'dhruvildadhich'
+EMAIL_HOST_USER= ''
+EMAIL_HOST_PASSWORD= ''
 EMAIL_PORT= 587
 
 
